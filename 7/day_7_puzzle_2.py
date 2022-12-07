250e7360a3af7f31f14503d77719e581d4dec478
Sam Humby <samalotmedia@gmail.com>
2022-12-07 10:47:38 +0000

    Day 7

--- 7/day_7_puzzle_2.py
+++ 7/day_7_puzzle_2.py
@@ -5,16 +5,20 @@ def run():
     dir_size_builder, dir_size = [], []
     for command in Reader.read("input"):
         if command[0] == "$":
+            # If "Move up" then remove directory from builder and add it's size to parent
             if command[5:7] == "..":
                 dir_size.append(dir_size_builder.pop())
                 dir_size_builder[-1] += dir_size[-1]
 
+            # If "New Directory" then add new count to the builder stack
             elif command[2] == "c":
                 dir_size_builder.append(0)
 
+        # If File - add it's size to the last element in the builder Stack
         elif not command[:3] == "dir":
             dir_size_builder[-1] += int(command.split(" ")[0])
 
+    # Add the remaining directories to the finished array
     for i in range(1, len(dir_size_builder)):
         dir_size.append(dir_size_builder.pop())
         dir_size_builder[-1] += dir_size[-1]
